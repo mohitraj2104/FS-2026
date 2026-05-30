2e300a51c9bbc02c8b854e1d50b025e19b6e337a
Mohit Raj <mohitrajrajput2104.com>
2026-05-30 11:59:05 +0530

    Upload project

--- DAY_11/DateObj.js
+++
@@ -1,8 +0,0 @@
-let date= new Date();
-console.log(date)
-let cdate = new Date("2026/05/17")
-console.log(cdate)
-console.log(cdate.toUTCString())
-console.log(cdate.getFullYear())
-console.log(cdate.getMonth())
-console.log(cdate.getHours())
--- DAY_11/Practice1.js
+++
@@ -1,5 +0,0 @@
-// USe Slice()
-
-let text = "JavaScript"
-let result = text.slice(0, 4);
-console.log(result) 
